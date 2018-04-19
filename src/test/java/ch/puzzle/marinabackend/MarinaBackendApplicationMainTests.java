08ad7895e11ad0bad78e1d6369bba0cc54d9cdff
Thomas Philipona <philipona@puzzle.ch>
2018-04-19 17:29:45 +0200

    adds Tests

---
+++ src/test/java/ch/puzzle/marinabackend/MarinaBackendApplicationMainTests.java
@@ -0,0 +1,15 @@
+package ch.puzzle.marinabackend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class MarinaBackendApplicationMainTests {
+
+	@Test
+	public void contextLoads() {
+		MarinaBackendApplication.main(new String[0]);
+	}
+
+}
--- src/test/java/ch/puzzle/marinabackend/employe/EmployeResourceTest.java
+++ src/test/java/ch/puzzle/marinabackend/employe/EmployeResourceTest.java
@@ -4,18 +4,22 @@ import static java.util.Collections.singletonList;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +28,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(EmployeResource.class)
@@ -75,15 +81,61 @@ public class EmployeResourceTest {
 	}
 	
 	@Test
-	@Ignore
-	public void shouldCreateEmploye() throws Exception {
+	public void shouldNotGetAnEmploye_notfound() throws Exception {
 		//given
-
-		when(employeRepository.save(housi)).thenReturn(housi);
+		when(employeRepository.findById(Long.valueOf(5))).thenReturn(Optional.of(housi));
 		// when then
-		mvc.perform(post("/employees").content(housi.toString()).contentType(APPLICATION_JSON)).andExpect(status().isOk());
-		// TODO: check location
+		mvc.perform(get("/employees/1").contentType(APPLICATION_JSON)).andExpect(status().isNotFound());
 
 	}
-
+	
+	@Test
+	public void shouldCreateEmploye() throws Exception {
+		//given
+		when(employeRepository.save(any(Employe.class))).thenReturn(housi);
+		
+		// when then
+		mvc.perform(post("/employees").param("id", housi.getId().toString())
+				.content(convertObjectToJsonBytes(housi)).contentType(APPLICATION_JSON))
+		.andExpect(status().isCreated())
+		.andExpect(redirectedUrlPattern("http://*/employees/"+ housi.getId().toString()));
+	}
+	
+	@Test
+	public void shouldUpdateEmploye() throws Exception {
+		//given
+		when(employeRepository.findById(housi.getId())).thenReturn(Optional.of(housi));
+		
+		// when then
+		mvc.perform(put("/employees/1").param("id", housi.getId().toString())
+				.content(convertObjectToJsonBytes(housi)).contentType(APPLICATION_JSON))
+		.andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void shouldNotUpdateEmploye_notfound() throws Exception {
+		//given
+		when(employeRepository.findById(housi.getId()-1)).thenReturn(Optional.of(housi));
+		
+		// when then
+		mvc.perform(put("/employees/1").param("id", housi.getId().toString())
+				.content(convertObjectToJsonBytes(housi)).contentType(APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void shouldDeleteEmploye() throws Exception {
+		//given
+		when(employeRepository.findById(housi.getId())).thenReturn(Optional.of(housi));
+		
+		// when then
+		mvc.perform(delete("/employees/1").param("id", housi.getId().toString()))
+		.andExpect(status().isOk());
+	}
+	
+	 
+    private byte[] convertObjectToJsonBytes(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsBytes(object);
+    }
 }
