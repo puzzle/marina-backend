35cacb9bba472d9ab1363affe6709cb5b263f8ed
Thomas Philipona <philipona@puzzle.ch>
2019-02-07 16:32:13 +0100

    Add Test Exception

--- src/main/java/ch/puzzle/marinabackend/app/SentryTestResource.java
+++ src/main/java/ch/puzzle/marinabackend/app/SentryTestResource.java
@@ -1,5 +1,15 @@
 package ch.puzzle.marinabackend.app;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SentryTestResource {
 
+    
+    @GetMapping("/sentrytestexception")
+    public void testException() {
+        throw new RuntimeException("sentry test exception");
+    }
+    
 }
