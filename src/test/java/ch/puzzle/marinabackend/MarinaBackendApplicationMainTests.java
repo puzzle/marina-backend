a5ba653914bdba37b94b819bea15e06aa330209a
retgal <galante@puzzle.ch>
2022-05-16 10:16:37 +0200

    Use test profile

--- src/test/java/ch/puzzle/marinabackend/MarinaBackendApplicationMainTests.java
+++ src/test/java/ch/puzzle/marinabackend/MarinaBackendApplicationMainTests.java
@@ -2,9 +2,11 @@ package ch.puzzle.marinabackend;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class MarinaBackendApplicationMainTests {
 
     @Test
